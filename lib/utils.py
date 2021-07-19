888ae07efb265499e85c91eaf33b0958a45a201d
jlehtomaa <jere.lehtomaa@gmail.com>
2021-07-19 19:19:07 +0200

    added probabilities.py

--- lib/utils.py
+++ lib/utils.py
@@ -37,3 +37,64 @@ def get_geoengineering_levels(states: list) -> dict:
         G[state.name] = state.geo_deployment_level
 
     return G
+
+
+def derive_effectivity(df: pd.DataFrame, players: list,
+                       states: list) -> np.ndarray:
+    """ Defines the effectivity correspondence.
+
+    For each possible proposer, every possible state transition, and
+    every possible other player as a responder, the effectivity matrix
+    has a value of 1 if that responder is in the approval committee, and
+    0 otherwise.
+
+    Note that the set of responders is the full set of players. That is, 
+    we also consider cases where a proposer can "propose" a transition to
+    itself. This is important, as in most settings countries might be able
+    to unilaterally exit their current coalition structure.
+
+    Arguments:
+      df: A DataFrame instance containing the strategies of all players.
+      players: A list (str) of all the players in the game.
+      states: A list (str) of all the considered states of the system.
+
+    Returns:
+      effectivity: a boolean array of the shape
+                   (n_players, n_players, n_states, n_states), corresponding
+                   to (proposer, responder, current_state, next_state)
+                   dimensions. Each entry tells whether the responder is
+                   a member of the approval committee, when the proposer
+                   suggests a transition from current_state to next_state.
+    """
+    n_players = len(players)
+    n_states = len(states)
+
+    effectivity = np.zeros((n_players, n_players, n_states, n_states))
+    # The dimensions are: [proposer, responder, current_state, next_state]
+
+    for prop_idx, proposer in enumerate(players):
+
+        # Consider all possible current states and proposed
+        # next states.
+        for current_state_idx, current_state in enumerate(states):
+            for next_state_idx, next_state in enumerate(states):
+                for responder in players:
+                    resp_idx = players.index(responder)
+
+                    resp_val = df.loc[(current_state, 'Acceptance', responder),
+                                      (f'Proposer {proposer}', next_state)]
+
+                    # If the corresponding cell is not empty, the player
+                    # is a member of the approval committee.
+                    is_member = int(~np.isnan(resp_val))
+
+                    effectivity[prop_idx, resp_idx, current_state_idx,
+                                next_state_idx] = is_member
+
+                # Trivially, the proposer must approve the transition,
+                # and is therefore included in the effectivity correspondence.
+                if current_state == next_state:
+                    effectivity[prop_idx, prop_idx, current_state_idx,
+                                next_state_idx] = 1
+
+    return effectivity
